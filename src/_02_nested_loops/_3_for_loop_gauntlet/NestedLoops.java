cb4eb149ec023a3d32d11585faeb11b2c37daeb2
root <root@f1b8df2b9f98>
2020-10-23 23:50:07 +0000

    Completed NestedLoops.

--- src/_02_nested_loops/_3_for_loop_gauntlet/NestedLoops.java
+++ src/_02_nested_loops/_3_for_loop_gauntlet/NestedLoops.java
@@ -4,12 +4,14 @@ public class NestedLoops {
 
 	public static void main(String[] args) {
 		
-		/*
+		
+		//Single For Loops
 		//1
 		for(int i = 0; i < 101; i++) {
 			System.out.println(i);
 		}
 		//2
+		/*
 		int a = 100;
 		for(int i = 0; i < 101; i++) {
 			System.out.println(a);
@@ -47,8 +49,52 @@ public class NestedLoops {
 		for(int i = 0; i < 14; i++) {
 			System.out.println("In " + a + ", I was " + i + " years old");
 			a = a + 1;
+		}
+		
+		//Nested For Loops
+		//1
+		int a = -1;
+		for(int i = 0; i < 2; i++) {
+			for(int j = 0; j < 2; j++) {
+				System.out.println(a + 1 + "  0");
+				System.out.println(a + 1 + "  1");
+				System.out.println(a + 1 + "  2");
+				for(int k = 0; k < 1; k++) {
+					a = a + 1;
+				}
+			}
+		}
+		//2
+		int a = 1;
+		for(int i = 0; i < 3; i++) {
+			int b = a + 1;
+			int c = a + 2;
+			System.out.println(a + " " + b + " " + c);
+			a = a + 3;
+		}
+		//3
+		int a = 1;
+		for(int i = 0; i < 10; i++) {
+			int b = a + 1;
+			int c = a + 2;
+			int d = a + 3;
+			int e = a + 4;
+			int f = a + 5;
+			int g = a + 6;
+			int h = a + 7;
+			int j = a + 8;
+			int k = a + 9;
+			System.out.println(a + " " + b + " " + c + " " + d + " " + e + " " + f + " " + g + " " + h + " " + j + " " + k);
+			a = a + 10;
+		}
+		//4
+		for(int i = 0; i < 6; i++) {
+			System.out.println("*");
 		}*/
-		//8
+		
+		
+		
+		
 		
 	}
 }
---
+++ src/_03_method_writing/_1_obedient_robot/ObedientRobot.java
@@ -0,0 +1,10 @@
+package _03_method_writing._1_obedient_robot;
+
+public class ObedientRobot {
+
+	public static void main(String[] args) {
+		
+		
+		
+	}
+}
